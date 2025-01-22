8e9cab9535a46c52ba6c41620679d7038039ebe9
Nagi <nagiitoshi@gmail.com>
2025-01-22 18:54:10 -0300

    Tests

--- src/test/java/nagi/itoshi/place_service/TestConfig.java
+++ src/test/java/nagi/itoshi/place_service/TestConfig.java
@@ -1,5 +1,26 @@
 package nagi.itoshi.place_service;
 
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.CompositeDatabasePopulator;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+
+import io.r2dbc.spi.ConnectionFactory;
+
+@TestConfiguration
 public class TestConfig {
+    @Bean
+    ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory){
+        ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+        initializer.setConnectionFactory(connectionFactory);
+
+        CompositeDatabasePopulator populator = new CompositeDatabasePopulator();
+        populator.addPopulators(new ResourceDatabasePopulator(new ClassPathResource("schema.sql")));
+        populator.addPopulators(new ResourceDatabasePopulator(new ClassPathResource("import.sql")));
+
+        return initializer;
+    }
     
 }
