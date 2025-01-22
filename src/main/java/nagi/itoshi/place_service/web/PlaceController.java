34191f7c26851f269de58acb6540e08876bd52a1
Nagi <nagiitoshi@gmail.com>
2025-01-22 16:20:31 -0300

    Controller OK

--- src/main/java/nagi/itoshi/place_service/web/PlaceController.java
+++ src/main/java/nagi/itoshi/place_service/web/PlaceController.java
@@ -1,8 +1,7 @@
 package nagi.itoshi.place_service.web;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,18 +9,51 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.validation.Valid;
 import nagi.itoshi.place_service.api.PlaceRequest;
 import nagi.itoshi.place_service.api.PlaceResponse;
+import nagi.itoshi.place_service.model.PlaceMapper;
 import nagi.itoshi.place_service.model.PlaceService;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
+
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 
 @RestController
 @RequestMapping("/places")
 public class PlaceController {
+    @Autowired
+    private PlaceService placeService;
 
+    @PostMapping
+    public ResponseEntity<Mono<PlaceResponse>> create(@Valid @RequestBody PlaceRequest request) {
+        var placeResponse = placeService.create(request).map(PlaceMapper::tResponse);
 
+        return ResponseEntity.status(HttpStatus.CREATED).body(placeResponse);
+    }
+
+    @PatchMapping("{id}")
+    public Mono<PlaceResponse> edit(@PathVariable("id") Long id, @RequestBody PlaceRequest request) {
+        return placeService.edit(id, request).map(PlaceMapper::tResponse);
+    }
+
+    @GetMapping("{id}")
+    public Mono<ResponseEntity<PlaceResponse>> get(@PathVariable("id") Long id){
+        return placeService.get(id)
+        .map(place -> ResponseEntity.ok(PlaceMapper.tResponse(place)))
+        .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public Flux<PlaceResponse> list(@RequestParam(required = false) String name){
+        return placeService.list(name).map(PlaceMapper::tResponse);
+    }
     
     
+    
+
 }
