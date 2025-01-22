e63ffb64aa7764e68d696a5d5575ccc92f0d157d
Nagi <nagiitoshi@gmail.com>
2025-01-22 13:18:04 -0300

    Repository

--- src/main/java/nagi/itoshi/place_service/model/PlaceRepository.java
+++ src/main/java/nagi/itoshi/place_service/model/PlaceRepository.java
@@ -1,5 +1,9 @@
 package nagi.itoshi.place_service.model;
 
-public class PlaceRepository {
+import org.springframework.data.repository.query.ReactiveQueryByExampleExecutor;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+public interface PlaceRepository extends ReactiveCrudRepository<Place, Long>, ReactiveQueryByExampleExecutor<Place>{
+
     
-}
+} 
