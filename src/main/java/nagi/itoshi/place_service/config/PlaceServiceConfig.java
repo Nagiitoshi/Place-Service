29dcd0baec8454bf3c16cbedb15fe329b28c8243
Nagi <nagiitoshi@gmail.com>
2025-01-22 18:32:04 -0300

    config

--- src/main/java/nagi/itoshi/place_service/config/PlaceServiceConfig.java
+++ src/main/java/nagi/itoshi/place_service/config/PlaceServiceConfig.java
@@ -1,5 +1,18 @@
 package nagi.itoshi.place_service.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
+
+import nagi.itoshi.place_service.model.PlaceRepository;
+import nagi.itoshi.place_service.model.PlaceService;
+
+@Configuration
+@EnableR2dbcAuditing
 public class PlaceServiceConfig {
+    @Bean
+    PlaceService placeService(PlaceRepository placeRepository){
+        return new PlaceService(placeRepository);
+    }
     
 }
