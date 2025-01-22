639a1cb7f096ec738f5ee89dcc0a855eee38aa20
Nagi <nagiitoshi@gmail.com>
2025-01-22 15:26:47 -0300

    PlaceRequest

--- src/main/java/nagi/itoshi/place_service/model/PlaceService.java
+++ src/main/java/nagi/itoshi/place_service/model/PlaceService.java
@@ -1,5 +1,21 @@
 package nagi.itoshi.place_service.model;
 
+import com.github.slugify.Slugify;
+
+import nagi.itoshi.place_service.api.PlaceRequest;
+import reactor.core.publisher.Mono;
+
 public class PlaceService {
+    private PlaceRepository placeRepository;
+    private Slugify slg;
+
+    public PlaceService(PlaceRepository placeRepository){
+        this.placeRepository = placeRepository;
+        this.slg = Slugify.builder().build();
+    }
+
+    public Mono<Place> create(PlaceRequest placeRequest){
+        var place = new Place(null,placeRequest.name(), null, null, null, null, null)
+    }
     
 }
