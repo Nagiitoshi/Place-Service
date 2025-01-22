f269f229ed2d545ad5f73c0f2f1dc8684062d742
Nagi <nagiitoshi@gmail.com>
2025-01-22 13:10:31 -0300

    Place Modifications

--- src/main/java/nagi/itoshi/place_service/model/Place.java
+++ src/main/java/nagi/itoshi/place_service/model/Place.java
@@ -1,5 +1,22 @@
 package nagi.itoshi.place_service.model;
 
-public class Place {
-    
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record Place(@Id Long id, @NotBlank String name, String slug,
+        @NotBlank String city,
+        @NotBlank String state,
+        @CreatedDate LocalDateTime createdAt,
+        @LastModifiedDate LocalDateTime updateAt) {
+    public Place whithSlug(String slug){
+        return new Place(id, slug, slug, slug, slug, createdAt, updateAt);
+
+
+    }
+
 }
