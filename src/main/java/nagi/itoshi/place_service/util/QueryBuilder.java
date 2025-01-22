3fe643c1a07a87c766a92936cecec31217f229f2
Nagi <nagiitoshi@gmail.com>
2025-01-22 18:23:48 -0300

    Query OK

--- src/main/java/nagi/itoshi/place_service/util/QueryBuilder.java
+++ src/main/java/nagi/itoshi/place_service/util/QueryBuilder.java
@@ -1,5 +1,19 @@
 package nagi.itoshi.place_service.util;
 
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+
+import nagi.itoshi.place_service.model.Place;
+
 public class QueryBuilder {
+    private QueryBuilder(){
+
+    }
+
+    public static Example<Place> makeQuery(Place planet){
+        ExampleMatcher exampleMatcher = ExampleMatcher.matchingAll().withIgnoreCase().withIgnoreNullValues();
+        
+        return Example.of(planet, exampleMatcher);
+    }
     
 }
