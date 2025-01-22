e100ab4032e3af53101204a18d9b3307a22ddcde
Nagi <nagiitoshi@gmail.com>
2025-01-22 15:45:38 -0300

    Mapper definid

--- src/main/java/nagi/itoshi/place_service/model/PlaceMapper.java
+++ src/main/java/nagi/itoshi/place_service/model/PlaceMapper.java
@@ -1,5 +1,22 @@
 package nagi.itoshi.place_service.model;
 
+import nagi.itoshi.place_service.api.PlaceRequest;
+import nagi.itoshi.place_service.api.PlaceResponse;
+
 public class PlaceMapper {
-    
+    public static Place updatePlaceFromDTO(PlaceRequest placeRequest, Place place) {
+        final String name = org.springframework.util.StringUtils.hasText(placeRequest.name()) ? placeRequest.name() : place.name();
+
+        final String city = org.springframework.util.StringUtils.hasText(placeRequest.city()) ? placeRequest.city() : place.city();
+
+        final String state = org.springframework.util.StringUtils.hasText(placeRequest.state()) ? placeRequest.state() : place.state();
+
+        return new Place(place.id(), name, place.slug(), city, state, place.createdAt(), place.updateAt());
+    }
+
+    public static PlaceResponse tResponse(Place place){
+        return new PlaceResponse(place.name(), place.slug(),
+        place.city(), place.state(), place.createdAt(), place.updateAt());
+    }
+
 }
